3e5eec1d5ca807848659286afc4c9cf8c65583c2
Paul Le Mercier <paul.lemercier05@gmail.com>
2022-03-19 14:57:27 +0100

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import '../style/App.css';
+
+// Importing Components
+import Form from './Form'
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Paul's Todo List</h1>
+      </header>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
