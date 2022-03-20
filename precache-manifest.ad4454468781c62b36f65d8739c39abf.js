361a2dffc7c50c57e3567a6d30296a3bea398406
Paul Le Mercier <paul.lemercier05@gmail.com>
2022-03-20 19:27:31 +0100

    Updates

---
+++ precache-manifest.ad4454468781c62b36f65d8739c39abf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b71c5a8a681a3f9ca9f",
+    "url": "/todo-react/static/css/main.07a5464d.chunk.css"
+  },
+  {
+    "revision": "1b71c5a8a681a3f9ca9f",
+    "url": "/todo-react/static/js/main.1b71c5a8.chunk.js"
+  },
+  {
+    "revision": "2998caf54286a93ccb62",
+    "url": "/todo-react/static/js/1.2998caf5.chunk.js"
+  },
+  {
+    "revision": "0bbd8ea5f85b3a7b208c",
+    "url": "/todo-react/static/js/2.0bbd8ea5.chunk.js"
+  },
+  {
+    "revision": "6513f053de8f6a6b328d",
+    "url": "/todo-react/static/js/runtime~main.6513f053.js"
+  },
+  {
+    "revision": "97c265b863f819a5585256299d183705",
+    "url": "/todo-react/index.html"
+  }
+];
